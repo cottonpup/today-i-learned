2ce8486eb96b18df234de9e45997751f9fce34b9
Emily <67526014+EmilyLee1206@users.noreply.github.com>
2020-12-17 01:18:50 +0900

    Create test.js

---
+++ docs/tutorials/유데미-자바스크립트-코스/test.js
@@ -0,0 +1,10 @@
+function printAge() {
+    if (true) {
+        var millenial = true;
+        const str = 'you are millenial';
+    }
+    // console.log(str); // error
+    // console.log(millenial); // true
+}
+
+printAge();
