4b4a1265730463d7201f890eb931da3f34992584
Emily <67526014+EmilyLee1206@users.noreply.github.com>
2021-01-03 16:48:44 +0900

    유데미 공부

--- docs/tutorials/유데미-자바스크립트-코스/test.js
+++ docs/tutorials/유데미-자바스크립트-코스/test.js
@@ -1,11 +1,21 @@
-let me = {
-  name: '람쥐',
-  thisInArrow: () => {
-    console.log('내 이름은 ' + this.name); // this 바인딩이 일어나지 않는다.
-  },
-  thisInRegular() {
-    console.log('내 이름은 ' + this.name); // this 바인딩이 일어난다.
-  }
+// 참조 방식, 복사 방식
+
+const a = {
+  name: 'emily',
+  age: 12
 };
-me.thisInArrow();
-me.thisInRegular();
+
+b = 4;
+
+
+function hello(first, second){
+
+  second = 10;
+
+  first.age = 26;
+
+console.log(`${first.name}은 나이를 먹어서 ${first.age}살이 되고 이제 ${second}해가 지났습니다..`);
+}
+
+hello(a, b);
+console.log(`${a.name}은 나이를 먹어서 ${a.age}살이 되고 이제 ${b}해가 지났습니다..`);
